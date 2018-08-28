50fc5f581a7b6b656d411c041935215c14a13a1f
duhang <840261937@qq.com>
2018-08-28 18:05:50 +0800

    归档..

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 axios.interceptors.request.use(res => {
     if (/^\/api\//.test(res.url)) {
-        res.url = `https://www.doinblog.com${res.url}`;
+        // res.url = `https://www.doinblog.com${res.url}`;
+        res.url = `http://127.0.0.1:3001${res.url.replace('/api', '')}`;
         return res;
     }
 });
@@ -50,6 +51,15 @@ module.exports = {
     async getLabelTotal() {
         let labelTotal = 0;
         let {code, data} = await axios.get('/api/label/getLabelTotal');
+        if (code === 200) {
+            labelTotal = data;
+        }
+        return labelTotal;
+    },
+    // 获取文章按月归档
+    async getArticleArchive() {
+        let labelTotal = 0;
+        let {code, data} = await axios.get('/api/article/archiveByMonth');
         if (code === 200) {
             labelTotal = data;
         }
--- router.js
+++ router.js
@@ -3,20 +3,38 @@ const api = require('./api');
 
 let router = new Router();
 
+async function getCommonData() {
+    let individuation = await api.getIndividuationData();
+    let nickname = individuation.nickname || '';
+    let self_intro = individuation.self_intro || '';
+    let portrait = individuation.portrait || '';
+    let articleTotal = await api.getArticleTotal();
+    let labelTotal = await api.getLabelTotal();
+    let banner = await api.getBannerData();
+    let businessCard = {nickname, self_intro, portrait, articleTotal, labelTotal};
+    let sideBar = {businessCard, banner};
+    return {individuation, sideBar};
+}
+
 router
     .get('/', async ctx => {
-        let getIndividuationData = await api.getIndividuationData();
-        let logo = getIndividuationData.logo || '';
-        let nickname = getIndividuationData.nickname || '';
-        let self_intro = getIndividuationData.self_intro || '';
-        let portrait = getIndividuationData.portrait || '';
-        let articleTotal = await api.getArticleTotal();
-        let labelTotal = await api.getLabelTotal();
-        let banner = await api.getBannerData();
-        let businessCard = {nickname, self_intro, portrait, articleTotal, labelTotal};
-        let sideBar = {businessCard, banner};
+        let commonData = await getCommonData();
         let articleList = await api.getArticleListData();
-        await ctx.render('./pages/index', {logo, banner, articleList, sideBar});
+        await ctx.render('./pages/index', {
+            individuation: commonData.individuation,
+            banner: commonData.sideBar.banner,
+            sideBar: commonData.sideBar,
+            articleList
+        });
+    })
+    .get('/archive', async ctx => {
+        let commonData = await getCommonData();
+        let articleArchive = await api.getArticleArchive();
+        await ctx.render('./pages/archive', {
+            individuation: commonData.individuation,
+            sideBar: commonData.sideBar,
+            articleArchive
+        });
     });
 
 module.exports = router;
